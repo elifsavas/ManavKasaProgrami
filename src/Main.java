2eefc5861743aff8e4a672454180cdb924b40f86
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-22 18:15:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner ;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double a = 2.14 , e = 3.67 , d = 1.11 , m = 0.95 , p = 5 ;
+        double kgA, kgE, kgD, kgM, kgP, tutar ;
+
+        Scanner input = new Scanner(System.in) ;
+
+        System.out.print("Armut (kg): ");
+        kgA = input.nextDouble() ;
+
+        System.out.print("Elma (kg):");
+        kgE = input.nextDouble() ;
+
+        System.out.print("Domates (kg):");
+        kgD = input.nextDouble() ;
+
+        System.out.print("Muz (kg):");
+        kgM = input.nextDouble() ;
+
+        System.out.print("Patlıcan (kg):");
+        kgP = input.nextDouble();
+
+        tutar = ( (a*kgA) + (e*kgE) + (d*kgD) + (m*kgM) + (p*kgP) );
+        System.out.println("Toplam:" + tutar + " TL");
     }
 }
